aa4eee768ab874eb37df61656654f8ab3474328b
So <1979510177@qq.com>
2019-07-02 00:28:20 +0800

    fix:build fail

--- doczrc.js
+++ doczrc.js
@@ -3,6 +3,7 @@ import { css } from 'docz-plugin-css'
 export default {
   title: 'Knight',
   description: 'A Design UI library for _So_',
+  codeSandbox: false,
   plugins: [
     css({
       preprocessor: 'postcss'
